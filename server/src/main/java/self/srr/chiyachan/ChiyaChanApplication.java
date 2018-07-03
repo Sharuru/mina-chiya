b494d799d05e6247840c38a6f287a7958270279a
Sharuru <mave@foxmail.com>
2018-07-03 16:57:45 +0900

    server infra

---
+++ server/src/main/java/self/srr/chiyachan/ChiyaChanApplication.java
@@ -0,0 +1,12 @@
+package self.srr.chiyachan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiyaChanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChiyaChanApplication.class, args);
+    }
+}
---
+++ server/src/test/java/self/srr/chiyachan/ChiyaChanApplicationTests.java
@@ -0,0 +1,16 @@
+package self.srr.chiyachan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChiyaChanApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
