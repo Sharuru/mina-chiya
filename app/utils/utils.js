5c74a63207f3d889a253c080389c1a711bbc610b
Sharuru <mave@foxmail.com>
2018-07-06 17:53:29 +0900

    add js throttle function

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -1,6 +1,7 @@
 import CONFIG from '../config.js'
 
-function apiRequest(endpoint, payload, callback){
+// API 请求的简单封装
+function apiRequest(endpoint, payload, callback) {
   wx.request({
     url: CONFIG.SERVER_URL + endpoint,
     data: payload,
@@ -10,12 +11,31 @@ function apiRequest(endpoint, payload, callback){
       'x-phone-userid': wx.getStorageSync("sessionId"),
       'content-type': 'application/json'
     },
-    complete: function(response){
+    complete: function(response) {
       callback && callback(response.data);
     }
   })
 }
 
+// 节流函数
+function throttle(func, gapTime) {
+  if (gapTime == null || gapTime == undefined) {
+    gapTime = 1500
+  }
+
+  let _lastTime = null
+
+  // 返回新的函数
+  return function() {
+    let _nowTime = +new Date()
+    if (_nowTime - _lastTime > gapTime || !_lastTime) {
+      func.apply(this, arguments) //将this和参数传给原函数
+      _lastTime = _nowTime
+    }
+  }
+}
+
 module.exports = {
-  apiRequest: apiRequest
+  apiRequest: apiRequest,
+  throttle: throttle
 }
